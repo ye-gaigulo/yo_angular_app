bec9c2158b252d16309306e5befc8588850858bd
Ernest Gaigulo <ernest@tangentsolutions.co.za>
2016-08-31 15:21:42 +0200

    edited karma.ci.conf.js, revert to PhantomJS

--- test/karma.ci.conf.js
+++ test/karma.ci.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
         // - PhantomJS
         // - IE (only Windows)
         browsers: [
-            "Chrome"
+            "PhantomJS"
         ],
 
         // Which plugins to enable
